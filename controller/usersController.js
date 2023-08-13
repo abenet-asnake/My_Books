eec43faff36e3d7e5fb5e3a4b093ec1f31d2e7fa
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-13 20:11:55 +0300

    user controller is done

---
+++ controller/usersController.js
@@ -0,0 +1,30 @@
+const asyncHandler= require('express-async-handler');
+//@disc registerUsers registering new users
+//@routes POST /api/users/register
+//@access public
+const registerUsers=asyncHandler(async(req,res)=>{
+    res.status(200).json({message: "New User Registered successfully"});
+});
+
+//@disc loginUsers 
+//@routes POST /api/users/login
+//@access public
+const loginUsers = asyncHandler(async(req,res)=>{
+    res.status(200).json({message: "You are Login successfully"});
+});
+
+//@disc currentUsers 
+//@routes POST /api/users/current
+//@access public
+const currentUsers = asyncHandler(async (req,res)=>{
+    res.status(200).json({message: "The Current User"});
+});
+
+//@disc logout 
+//@routes GET /api/users/logout
+//@access public
+const logoutUser = asyncHandler(async (req,res)=>{
+    res.status(200).json({message: "The Current User"});
+});
+
+module.exports ={registerUsers,loginUsers,currentUsers,loginUsers};

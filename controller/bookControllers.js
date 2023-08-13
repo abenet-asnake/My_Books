e34e033c609e3bbb02f742d63df3751d087b4262
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-13 19:35:19 +0300

    installing the express-async-handler

---
+++ controller/bookControllers.js
@@ -0,0 +1,39 @@
+const asyncHandler= require('async-express-handler');
+//@disc getBooks get all books
+//@routes GET /api/books
+//@access public
+
+
+//@disc getBooksByID get Books by ID
+//@routes GET /api/books
+//@access public
+
+
+//@disc getBooksByAuthor get Books by ID
+//@routes GET /api/books
+//@access public
+
+
+//@disc getBooksByTittle get Books by ID
+//@routes GET /api/books
+//@access public
+
+
+//@disc addBooks Add a new Book
+//@routes POST /api/books/add
+//@access public
+
+
+//@disc addBooks Add a new Book
+//@routes POST /api/books/add
+//@access public
+
+
+//@disc editBooks Edit a Book
+//@routes PUT /api/books/
+//@access public
+
+
+//@disc deleteBooks Delete a Book
+//@routes DELETE /api/books/add
+//@access public
